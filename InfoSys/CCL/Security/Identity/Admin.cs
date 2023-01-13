0a2ce83d8aa56e2f9b3a90f880d36bb812e23352
DanilDziuba <danilpuskin791@gmail.com>
2023-01-13 17:06:36 +0200

    CCL

---
+++ InfoSys/CCL/Security/Identity/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCL.Security.Identity
+{
+    public class Admin
+        : Worker
+    {
+        public Admin(int UID1, string Email1, int DistrictID1)
+            : base(UID1, Email1, nameof(Admin), DistrictID1)
+        {
+        }
+    }
+}
---
+++ InfoSys/CCL/Security/Identity/Owner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCL.Security.Identity
+{
+    public class Owner
+        : Resident
+    {
+        public Owner(int UID1, string Email1)
+            : base(UID1, Email1, nameof(Owner))
+        {
+        }
+    }
+}
